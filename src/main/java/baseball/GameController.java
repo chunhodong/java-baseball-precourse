ed60b7a6c2b358e460e9ff51345ea466ec217762
chunhodong <yhms4432@naver.com>
2022-09-30 21:22:06 +0900

    fix: 게임시작기능추가
    메소드구조만작성

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,4 +1,35 @@
 package baseball;
 
 public class GameController {
+
+    private static final String START = "1";
+    private static final String END = "2";
+    private GameModel model;
+    private GameView view;
+    private boolean isProgress;
+
+
+    public GameController(GameModel model, GameView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+
+    public void onStart() {
+
+
+    }
+
+    private void onRunning() {
+
+        while (onProgress()) {
+
+        }
+
+    }
+
+    private boolean onProgress() {
+        return isProgress;
+    }
+
 }
